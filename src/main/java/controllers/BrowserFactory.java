ae51faf9a8b58c2c479103f9b229add775a6326e
Komireddy Chandu Reddy <komireddychandureddy@gmail.com>
2024-07-26 21:03:16 +0100

    Updated to latest versions

--- src/main/java/controllers/BrowserFactory.java
+++ src/main/java/controllers/BrowserFactory.java
@@ -1,13 +1,11 @@
 package controllers;
 
-import java.util.concurrent.TimeUnit;
-
+import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -20,7 +18,6 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BrowserFactory extends InitMethod
 {
-	@SuppressWarnings("deprecation")
 	static WebDriver createDriver(String browser, String url) throws Exception
 	{
 		WebDriver driver;
@@ -29,82 +26,47 @@ public class BrowserFactory extends InitMethod
 		switch(browser.toLowerCase())
 		{
 		case "chrome":
-			System.setProperty("webdriver.chrome.driver", "src/main/resources/Drivers/chromedriver.exe");
-			//System.setProperty("webdriver.chrome.driver", "D:\\github\\Java\\TestAutomation\\src\\main\\resources\\Drivers\\chromedriver.exe");
-			WebDriverManager.chromedriver().setup();
+			//WebDriverManager.chromedriver().setup();
 			driver = new ChromeDriver();
-			//driver.manage().deleteAllCookies();
-			driver.manage().window().maximize();
-			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-			//driver.get(url);
 			break;
 			
 		case "chrome_headless":
-			//System.setProperty("webdriver.chrome.driver", "src/main/resources/Drivers/chromedriver.exe");
-			WebDriverManager.chromedriver().setup();
+			//WebDriverManager.chromedriver().setup();
 			ChromeOptions chromeOptions = new ChromeOptions();
 			chromeOptions.addArguments("--headless");  
 			chromeOptions.addArguments("--disable-gpu");  
 			driver = new ChromeDriver(chromeOptions);
-			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-			//driver.get(url);
 			break;
 
 		case  "firefox":
-			//System.setProperty("webdriver.gecko.driver", "src/main/resources/Drivers/geckodriver.exe");
-			WebDriverManager.firefoxdriver().setup();
+			//WebDriverManager.firefoxdriver().setup();
 			driver = new FirefoxDriver();
-			driver.manage().deleteAllCookies();
-			driver.manage().window().maximize();
-			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		//	driver.get(url);
+
 			break;
 
 		case  "ie":
-			//System.setProperty("webdriver.ie.driver", "src/main/resources/Drivers/IEDriverServer.exe");
-			WebDriverManager.iedriver().setup();
+			//WebDriverManager.iedriver().setup();
 			driver = new InternetExplorerDriver();
-			driver.manage().deleteAllCookies();
-			driver.manage().window().maximize();
-			driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-			//driver.get(url);
+
 			break;	
 
 		case  "edge":
-			//System.setProperty("webdriver.edge.driver", "src/main/resources/Drivers/MicrosoftWebDriver.exe");
-			WebDriverManager.edgedriver().setup();
+			//WebDriverManager.edgedriver().setup();
 			driver = new EdgeDriver();
-			driver.manage().deleteAllCookies();
-			driver.manage().window().maximize();
-			driver.manage().timeouts().implicitlyWait(610, TimeUnit.SECONDS);
-		//	driver.get(url);
+
 			break;
 
-		case  "unit":
-			driver = new HtmlUnitDriver();
-			driver.manage().window().maximize();
-			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		//	driver.get(url);
-			break;
-
-		case  "opera":
-			//System.setProperty("webdriver.chrome.driver", "src/main/resources/Drivers/chromedriver.exe");
-			WebDriverManager.operadriver().setup();
-			capabilities = DesiredCapabilities.opera();
-			ChromeOptions optionsOpera = new ChromeOptions();
-			optionsOpera.setBinary("C:/Program Files/Opera/launcher.exe");
-			capabilities.setCapability(ChromeOptions.CAPABILITY, optionsOpera);
-			driver = new ChromeDriver(capabilities);
-			driver.manage().window().maximize();
-			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-			//driver.get(url);
-			break;
+		/*
+		 * case "unit": driver = new HtmlUnitDriver();
+		 */
+	
 
 		default:
 			throw new Exception("Please Provide a Valid Browser");
 		}
+			driver.manage().deleteAllCookies();
 			driver.manage().window().maximize();
-			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 			driver.get(url);
 		return driver;		
 	}
